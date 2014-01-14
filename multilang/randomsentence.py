27182a69b85dbe56453417bae3cabf54ac3a2b66
charlielin <charlielin6@foxmail>
2014-01-14 16:45:13 +0800

    add randomsentence.py

---
+++ multilang/randomsentence.py
@@ -0,0 +1,20 @@
+import time
+import random
+import storm
+
+class RandomSentenceSpout(storm.Spout):
+    sentences = [
+        "the cow jumped over the moon",
+        "an apple a day keeps the doctor away",
+        "four score and seven years ago",
+        "snow white and the seven dwarfs",
+        "i am at two with nature"
+    ]
+    def nextTuple(self):
+        time.sleep(1.00)
+        sentence = self.sentences[random.randint(0, len(self.sentences) - 1)]
+        storm.log('RandomSentenceSpout, storm.emit: ' + sentence)
+        storm.emit([sentence])
+
+
+RandomSentenceSpout().run()
