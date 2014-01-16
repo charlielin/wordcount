82d95412124bb62dbd38f6ef0aee2248b8b8ff1f
charlielin <charlielin6@foxmail.com>
2014-01-16 11:15:52 +0800

    add read TopologyStructure done!

--- src/main/java/example/multilangtopo/WordCountTopology.java
+++ src/main/java/example/multilangtopo/WordCountTopology.java
@@ -4,24 +4,27 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.task.ShellBolt;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.IRichBolt;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.*;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import example.multilangtopo.spout.RandomSentenceShellSpout;
 import example.multilangtopo.bolt.SplitSentenceShellBolt;
 import example.multilangtopo.bolt.WordCountShellBolt;
 
+import example.multilangtopo.utils.BaseComponent;
+import example.multilangtopo.utils.BoltComponent;
 import example.multilangtopo.utils.LoadTopologyStructure;
 import example.multilangtopo.utils.SpoutComponent;
+import org.apache.log4j.Logger;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Lin QiLi
@@ -29,6 +32,7 @@ import example.multilangtopo.utils.SpoutComponent;
  * Time: 上午11:11
  */
 public class WordCountTopology {
+    private static Logger _LOG = Logger.getLogger(WordCountTopology.class);
     public static class WordCount extends BaseBasicBolt {
         Map<String, Integer> counts = new HashMap<String, Integer>();
 
@@ -50,7 +54,17 @@ public class WordCountTopology {
 
     public static void main(String[] args) throws Exception {
 
-        LoadTopologyStructure.getTopologyComponent();
+        List<BaseComponent[]> baseComponentsList = LoadTopologyStructure.getTopologyComponent();
+        SpoutComponent[] spoutComponents = (SpoutComponent[]) baseComponentsList.get(0);
+        BoltComponent[] boltComponents = (BoltComponent[]) baseComponentsList.get(1);
+        for (SpoutComponent spoutComponent : spoutComponents) {
+            _LOG.info("ComponentName: "+spoutComponent.getComponent_name());
+            _LOG.info("ClassName: "+spoutComponent.getClass_name());
+            _LOG.info("TaskNum: "+spoutComponent.getTask_num());
+        }
+        for (BoltComponent boltComponent : boltComponents) {
+            _LOG.info("GroupingArgs: "+ Arrays.toString(boltComponent.getGrouping_args()));
+        }
         TopologyBuilder builder = new TopologyBuilder();
 
         builder.setSpout("spout", new RandomSentenceShellSpout(), 5/*BoltNum*/).setNumTasks(5);
