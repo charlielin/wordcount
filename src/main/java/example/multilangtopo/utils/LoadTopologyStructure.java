75db0c2274b64d9d6410d80f464c30fb177b4dad
charlielin <charlielin6@foxmail.com>
2014-01-16 10:35:01 +0800

    add read TopologyStructure done!

--- src/main/java/example/multilangtopo/utils/LoadTopologyStructure.java
+++ src/main/java/example/multilangtopo/utils/LoadTopologyStructure.java
@@ -3,7 +3,9 @@ package example.multilangtopo.utils;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,27 +20,9 @@ public class LoadTopologyStructure {
 
     private static final Logger _LOG = Logger.getLogger(LoadTopologyStructure.class);
 
-    public static void getTopologyComponent() throws IOException {
+    public static List<BaseComponent[]> getTopologyComponent() throws IOException {
         LoadResources topologyStructure = new LoadResources("topology.json");
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode root = mapper.readTree(topologyStructure.getParameterInputStream());
-//        _LOG.info("root NodeType: "+root.getNodeType());
-//        Iterator<String> componentTypes = root.fieldNames();
-//        while (componentTypes.hasNext()) {
-//            String componentType = componentTypes.next();
-//            _LOG.info("current Component: "+componentType);
-//
-//        }
-//        JsonNode spoutRoot = root.get("Spout");
-//        _LOG.info("spout's NodeType: " + spoutRoot.getNodeType()); //ARRAY
-//        Iterator<JsonNode> spoutComponents = spoutRoot.iterator();
-//        while (spoutComponents.hasNext()) {
-//            JsonNode spoutComponent = spoutComponents.next();
-//            _LOG.info("spoutComponentType: " + spoutComponent.getNodeType());
-//            Iterator<JsonNode> spoutComponentParas = spoutComponent.
-//
-//        }
-//        JsonParser jsonParser = jsonFactory.createJsonParser(topologyStructure.getParameterInputStream());
+
         ObjectMapper mapper = new ObjectMapper();
         TopologyComponent topologyComponent = mapper.readValue(topologyStructure.getParameterInputStream(), TopologyComponent.class);
         SpoutComponent[] spoutComponents = topologyComponent.getSpout();
@@ -54,6 +38,11 @@ public class LoadTopologyStructure {
             _LOG.info("GroupingArgs: "+ Arrays.toString(boltComponent.getGrouping_args()));
         }
 
+        List<BaseComponent[]> returnList = new ArrayList<BaseComponent[]>();
+        returnList.add(0, spoutComponents);
+        returnList.add(1, boltComponents);
+
+        return  returnList;
     }
 
 }
